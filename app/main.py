18119e1422d7dd27486eb66c186fdcc81b26c64d
Md Kasim Ansari <126614695+Kasimalbatros@users.noreply.github.com>
2025-09-07 01:59:55 +0530

    Update main.py
    updated

--- app/main.py
+++ app/main.py
@@ -11,4 +11,4 @@ def health():
     return jsonify({"status": "healthy"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8081, debug=False)
